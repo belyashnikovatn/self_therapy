194931f5b7547d6b208f5046927f3ab7b065ed8d
Tania Belyashnikova <belyashnikova.tn@gmail.com>
2024-07-16 16:41:23 +0500

    add Person model

--- self_therapy/selfcare/models.py
+++ self_therapy/selfcare/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    """Persons profile: just minimum."""
+
+    name = models.CharField(
+        max_length=256,
+        verbose_name='ФИО пользователя',
+    )
+    tlg_id = models.CharField(
+        max_length=256,
+        verbose_name='ID диалога пользователя',
+    )
+
+
+class PresetsHelpTips(models.Model):
+    """Initial tips for self-help."""
+
+    text = models.TextField(
+        verbose_name='Текстовое описание подсказки'
+    )
+
+
+class PersonsHelpTips(models.Model):
+    """Help tips for particular person."""
+
+    text = models.TextField(
+        verbose_name='Текстовое описание подсказки'
+    )
+    is_on = models.BooleanField(
+        default=True,
+        verbose_name='Показывать'
+    )
+    likeliness = models.DecimalField(
+        max_digits=1,
+        decimal_places=2,
+        default=0.50
+        
+    )
+
+
