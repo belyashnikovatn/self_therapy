e732ebe4b48e797f929432340682417251f1cb14
Tania Belyashnikova <belyashnikova.tn@gmail.com>
2024-07-17 11:28:47 +0500

    try to use telegram_django_bot

--- self_therapy/selfcare/views.py
+++ self_therapy/selfcare/views.py
@@ -1,1 +1,16 @@
-from django.shortcuts import render
+from telegram_django_bot import forms as td_forms
+from telegram_django_bot.td_viewset import TelegramViewSet
+
+from selfcare.models import PersonsHelpTips
+
+
+class PersonsHelpTipsForm(td_forms.TelegramModelForm):
+    class Meta:
+        model = PersonsHelpTips
+        fields = ['text',]
+
+
+class PersonsHelpTipsViewSet(TelegramViewSet):
+    model_form = PersonsHelpTipsForm
+    queryset = PersonsHelpTips.objects.all()
+    viewset_name = 'PersonsHelpTips'
