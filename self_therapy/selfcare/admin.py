7bb4a6bb83e3dcf6b797bff0b93f260b49546500
Tania Belyashnikova <belyashnikova.tn@gmail.com>
2024-07-17 09:00:25 +0500

    add admin classes

--- self_therapy/selfcare/admin.py
+++ self_therapy/selfcare/admin.py
@@ -1,3 +1,30 @@
+"""Register your models here."""
 from django.contrib import admin
 
-# Register your models here.
+from selfcare.models import Person, PersonsHelpTips, PresetsHelpTips
+
+
+class CustomModelAdmin(admin.ModelAdmin):
+    """Custom base for all classes."""
+
+    def __init__(self, model, admin_site):
+        """For the list display."""
+        self.list_display = [
+            field.name for field in model._meta.fields if field.name != 'id'
+            ]
+        super(CustomModelAdmin, self).__init__(model, admin_site)
+
+
+@admin.register(Person)
+class PersonAdmin(CustomModelAdmin):
+    """Admin Person."""
+
+
+@admin.register(PresetsHelpTips)
+class PresetsHelpTipsAdmin(CustomModelAdmin):
+    """Admin PresetsHelpTips."""
+
+
+@admin.register(PersonsHelpTips)
+class PersonsHelpTipsAdmin(CustomModelAdmin):
+    """Admin PersonsHelpTips."""
