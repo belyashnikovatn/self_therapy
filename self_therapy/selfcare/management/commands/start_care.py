104d3f7658dee649df90e7039972b78975ea5ef0
Tania Belyashnikova <belyashnikova.tn@gmail.com>
2024-07-19 14:13:09 +0500

    add InlineKeyboardButton

--- self_therapy/selfcare/management/commands/start_care.py
+++ self_therapy/selfcare/management/commands/start_care.py
@@ -5,7 +5,7 @@ import requests
 from dotenv import load_dotenv
 from django.core.management.base import BaseCommand
 from telebot import TeleBot, types
-from selfcare.models import Person, PersonsHelpTips, PresetsHelpTips
+from selfcare.models import Person, PersonsHelpTip, PresetHelpTip, Emotion
 
 load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
@@ -19,9 +19,53 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         """Make a bot and start work."""
         bot = TeleBot(token=BOT_TOKEN)
+        # start_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        start_keyboard = types.InlineKeyboardMarkup()
+        # btn_create_user = types.KeyboardButton('/createuser')
+        # btn_loadpreset = types.KeyboardButton('/loadpreset')
+        # btn_getemotion = types.KeyboardButton('/getemotion')
+        # btn_getsupport = types.KeyboardButton('/getsupport')
+        btn_addsupport = types.InlineKeyboardButton('Test',
+                                                    callback_data='addsupport')
+        start_keyboard.add(btn_addsupport)
+        # start_keyboard.add(btn_create_user, btn_loadpreset,
+        #                    btn_getemotion, btn_getsupport,
+        #                    btn_addsupport)
 
-        @bot.message_handler(commands=['loadsets'])
-        def load_presets(message):
+        @bot.callback_query_handler(func=lambda call: True)
+        def add_support(call):
+            if call.data == 'addsupport':
+                bot.send_message(call.from_user.id, text='Test add supp')
+            else:
+                bot.send_message(call.from_user.id, text='Test not add supp')
+
+            # chat_id = message.chat.id
+            # name = message.chat.first_name
+            # # пока сделала так, чтобы рандомно, но нужно переделать
+            # person, _ = Person.objects.get_or_create(
+            #     tlg_id=chat_id,
+            #     defaults={
+            #         'name': name,
+            #     }
+            # )
+            # tip = person.personstips.order_by('?').first()
+            # text = tip.text
+            # bot.send_message(chat_id, text=text)
+
+        @bot.message_handler(commands=['createuser'])
+        def create_user(message):
+            chat_id = message.chat.id
+            name = message.chat.first_name
+            Person.objects.get_or_create(
+                tlg_id=chat_id,
+                defaults={
+                    'name': name,
+                }
+            )
+            bot.send_message(chat_id, text=f'User {name} created!')
+
+        @bot.message_handler(commands=['loadpreset'])
+        def load_preset(message):
             chat_id = message.chat.id
             name = message.chat.first_name
 
@@ -31,53 +75,48 @@ class Command(BaseCommand):
                     'name': name,
                 }
             )
-            helptips = PresetsHelpTips.objects.all()
+            helptips = PresetHelpTip.objects.all()
             bulk_list = []
             for tip in helptips:
                 bulk_list.append(
-                    PersonsHelpTips(
+                    PersonsHelpTip(
                         text=tip.text,
                         person=person,
                         preset_tip=tip
                     )
                 )
-            PersonsHelpTips.objects.bulk_create(bulk_list)
-            bot.send_message(chat_id, text='Presets loaded')
+            PersonsHelpTip.objects.bulk_create(bulk_list)
+            bot.send_message(chat_id, text='Presets loaded!')
 
-        @bot.message_handler(commands=['need_support'])
+        @bot.message_handler(commands=['getemotion'])
+        def get_emotion(message):
+            emotion = Emotion.objects.order_by('?').first()
+            text = emotion.name + '\n' + emotion.descriprion
+            bot.send_message(message.chat.id, text=text)
+
+        @bot.message_handler(commands=['getsupport'])
         def get_support(message):
             chat_id = message.chat.id
+            name = message.chat.first_name
             # пока сделала так, чтобы рандомно, но нужно переделать
-            tip = PersonsHelpTips.objects.order_by('?').first()
-            text = tip.text
-            bot.send_message(chat_id, text=text)
-
-        @bot.message_handler(commands=['start'])
-        def wake_up(message):
-            chat_id = message.chat.id
-            chat = message.chat
-            name = chat.first_name
-
-            Person.objects.get_or_create(
+            person, _ = Person.objects.get_or_create(
                 tlg_id=chat_id,
                 defaults={
                     'name': name,
                 }
             )
-            keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-            button_loadsets = types.KeyboardButton('/loadsets')
-            button_getsupport = types.KeyboardButton('/need_support')
-            button_moodtracker = types.KeyboardButton('/moodtracker')
-            button_selfesteem = types.KeyboardButton('/selfesteem')
-            keyboard.add(button_loadsets)
-            keyboard.add(button_getsupport)
-            keyboard.add(button_moodtracker)
-            keyboard.add(button_selfesteem)
+            tip = person.personstips.order_by('?').first()
+            text = tip.text
+            bot.send_message(chat_id, text=text)
+
+        @bot.message_handler(commands=['start', 'hi'])
+        def start_up(message):
+            chat = message.chat
 
             bot.send_message(
                 chat_id=chat.id,
-                text=f'Привет, {name}! Воспользуйтесь меню:',
-                reply_markup=keyboard,
+                text='Это режим разработки, поэтому все кнопки пока общим списком',
+                reply_markup=start_keyboard
             )
 
         bot.infinity_polling()
