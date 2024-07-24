b4842297d2675a89d7b027206ef9c11ea8048119
belyashnikovatn <101242667+belyashnikovatn@users.noreply.github.com>
2024-07-24 14:55:13 +0500

    change views

--- self_therapy/selfcare/views.py
+++ self_therapy/selfcare/views.py
@@ -1,16 +1,4 @@
 # from telegram_django_bot import forms as td_forms
 # from telegram_django_bot.td_viewset import TelegramViewSet
 
-# from selfcare.models import PersonsHelpTips
-
-
-# class PersonsHelpTipsForm(td_forms.TelegramModelForm):
-#     class Meta:
-#         model = PersonsHelpTips
-#         fields = ['text',]
-
-
-# class PersonsHelpTipsViewSet(TelegramViewSet):
-#     model_form = PersonsHelpTipsForm
-#     queryset = PersonsHelpTips.objects.all()
-#     viewset_name = 'PersonsHelpTips'
+from selfcare.models import Person, PersonsHelpTip, PresetHelpTip
