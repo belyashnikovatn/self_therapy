ab3d03ef261f15f3c70f001b3972fb6a3bceede9
Tania Belyashnikova <belyashnikova.tn@gmail.com>
2024-11-02 10:40:24 +0500

    From the scratch

--- self_therapy/selfcare/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SelfcareConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'selfcare'
--- self_therapy/selfcare/views.py
+++
@@ -1,4 +0,0 @@
-# from telegram_django_bot import forms as td_forms
-# from telegram_django_bot.td_viewset import TelegramViewSet
-
-from selfcare.models import Person, PersonsHelpTip, PresetHelpTip
