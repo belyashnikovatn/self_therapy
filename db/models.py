bf1d62cfb08cba0fca932d2c55e67a8730c90e8a
Tania Belyashnikova <belyashnikova.tn@gmail.com>
2024-11-06 08:57:51 +0500

    Add operation Add_note

--- db/models.py
+++ db/models.py
@@ -1,17 +1,9 @@
 """Models declarations."""
 
-import enum
-
-from sqlalchemy import BigInteger, Enum, ForeignKey, Integer, String, Text
+from sqlalchemy import BigInteger, ForeignKey, Integer, String, Text
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from db.database import Base
-
-
-class Kind(enum.Enum):
-    helptip: str
-    mood: str
-    selfesteem: str
 
 
 class User(Base):
@@ -41,9 +33,9 @@ class Note(Base):
     user_id: Mapped[int] = mapped_column(
         ForeignKey('user.id'), nullable=False)
     text: Mapped[str] = mapped_column(Text, nullable=False)
-    kind: Mapped[str] = mapped_column(Enum(Kind))
+    type: Mapped[str] = mapped_column(String, nullable=False)
     user: Mapped['User'] = relationship('User', back_populates='notes')
 
     def __repr__(self) -> str:
-        return (f'Запись типа {self.kind} у'
+        return (f'Запись типа {self.type} у'
                 f'пользователя {self.user_id}')
--- db/operations.py
+++ db/operations.py
@@ -8,20 +8,39 @@ from sqlalchemy.exc import SQLAlchemyError
 
 
 @connection
-async def set_user(session, tg_id: int, username: str, full_name: str):
+async def set_user(session, tg_id, username, full_name):
     try:
         user = await session.scalar(select(User).filter_by(id=tg_id))
         if not user:
             new_user = User(id=tg_id, username=username, full_name=full_name)
             session.add(new_user)
             await session.commit()
-            logger.info(f'User {tg_id} is registred')
+            logger.info(f'Пользователь {tg_id} зареган')
             return None
         else:
-            logger.info(f'User {tg_id} found!')
+            logger.info(f'Пользователь {tg_id} найден')
             return user
     except SQLAlchemyError as e:
-        logger.error(f'Error!! {e}')
+        logger.error(f'Ошибка! Смотри {e}')
         await session.rollback()
 
 
+@connection
+async def add_note(session, user_id, type, text):
+    try:
+        user = await session.scalar(select(User).filter_by(id=user_id))
+        if not user:
+            logger.error(f'Пользователь с {id} не найден')
+            return None
+        new_note = Note(
+            user_id=user_id,
+            text=text,
+            type=type,
+        )
+        session.add(new_note)
+        await session.commit()
+        logger.info(f'Запись типа {type} у {user_id} добавлена')
+        return new_note
+    except SQLAlchemyError as e:
+        logger.error(f'Ошибка! Смотри {e}')
+        await session.rollback()
